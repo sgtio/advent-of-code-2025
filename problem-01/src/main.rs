2362eca8b9945d81469ef180d3782dce20d71f18
Sergio <sejoruiz@gmail.com>
2025-12-21 13:31:17 +0100

    Add solution for problem 01 part 2

--- problem-01/src/main.rs
+++ problem-01/src/main.rs
@@ -7,6 +7,7 @@ fn main() -> io::Result<()> {
 
     let mut position = 50;
     let mut password = 0;
+    let mut password_pt2 = 0;
     let lines = io::stdin().lines();
     for line in lines {
         let mut data = line.unwrap();
@@ -26,23 +27,39 @@ fn main() -> io::Result<()> {
                 panic!("Unexpected rotation: {rotation}");
             }
         };
-        
+
         /* Then parse the offset */
-        let offset:i32 = data.parse().expect("Failed to parse string to integer: {data}");
-        
-        /* And calculate the new position. Assume offset will not overflow integer */
-        position = (position + offset * direction) % 100;
+        let mut offset:i32 = data.parse().expect("Failed to parse string to integer: {data}");
+
+        /* For part 2, we want to see how many full circles we do with the offset */
+        password_pt2 += offset/100;
+        offset = offset % 100;
+
+        /* And calculate the new position. */
+        position += offset * direction;
+        trace!("Position: {position}, offset: {offset}, direction: {direction}");
+
+        /* If we passed through 0, and we were not already in 0, add to the part 2 password */
+        if (position <= 0 || position >= 100) && position != offset * direction {
+            trace!("Password part 2 increase");
+            password_pt2 += 1;
+        }
+
+        /* Calculate the resulting position */
+        position = position % 100;
         if position < 0 {
             position += 100;
         }
-        trace!("Position: {position}, offset: {offset}, direction: {direction}");
-        
+        trace!("Final position: {position}");
+
+        /* And if we landed in 0, update part 1 password */
         if position == 0 {
             trace!("Password increase!");
             password += 1;
         }
     }
-    
-    println!("Password is {password}");
+
+    println!("Password for part 1 is {password}");
+    println!("Password for part 2 is {password_pt2}");
     Ok(())
 }
